f37430ff976e070107e1c0b9aa95d7ccc40eeb1c
Blas <blasillous@gmail.com>
2024-09-18 00:10:10 +0200

    Oracle datos dockerizado

--- src/main/java/es/jcyl/eclap/colapp/oad/ConexionDb.java
+++ src/main/java/es/jcyl/eclap/colapp/oad/ConexionDb.java
@@ -5,12 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDb {
-	
-	static String JDBC_DRIVER ="org.h2.Driver";
-	
-	public static Connection obtenerConexionDb() throws SQLException, ClassNotFoundException {		
-		Class.forName(JDBC_DRIVER);		
-		Connection conn = DriverManager.getConnection("jdbc:h2:mem:colappdb;SCHEMA=COLAPP","sa","");
+	static String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+	static String DB_URL = "jdbc:oracle:thin:@oracle:1521:FREE";
+	static String USER = "colapp"; // Reemplaza con tu usuario de Oracle
+	static String PASS = "colapp"; // Reemplaza con tu contraseña de Oracle
+
+
+	public static Connection obtenerConexionDb() throws SQLException, ClassNotFoundException {
+		Class.forName(JDBC_DRIVER);
+
+		Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		return conn;
 	}
 
